0a2dfe00a50fbe82acc40e75689374c13cececf2
LiftBlazinFire <124742770+LiftBlazinFire@users.noreply.github.com>
2023-10-01 19:02:48 -0400

    Add files via upload

---
+++ Final Project.py
@@ -0,0 +1,31 @@
+# Keagan Centeno
+# 
+# 
+
+
+
+#define a Variable to get custormer's name 
+name = input("Enter The Name for Your Order: ")
+print (name)
+# varible for loop function for selection
+number = int(input("Enter The Best Looking Combo: "))
+
+#define a function to get user's selection from choices 
+if number <= 1:
+    print("You have chosen Combo 1")
+elif number <= 2:
+    print("You have chosen combo 2")
+elif  number <=3 :
+    print("You have chosen number 3")
+elif number <=4:
+    print("You have chosen number 4")
+else:
+    print("This is not a Valid combo number")
+
+    
+    
+
+    
+   
+    
+
