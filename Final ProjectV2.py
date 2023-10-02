e34b1856f056d5c68636c19ef2c5c8117a8197f4
LiftBlazinFire <124742770+LiftBlazinFire@users.noreply.github.com>
2023-10-02 09:41:34 -0400

    Add files via upload

---
+++ Final ProjectV2.py
@@ -0,0 +1,26 @@
+# Keagan Centeno
+# 
+# Final Project
+import tkinter as tk
+window = tk.Tk()
+window.title("Welcome to Skyline")
+
+title_label = tk.Label(window, text=("Getting Your Order"), font=("Arial", 20))
+title_label.pack()
+#define a label 
+
+#name = input("Enter The Name for Your Order: ")
+#print (name)
+#define something for the selections 
+# define selections buttons 
+#number = int(input("Enter The Best Looking Combo: "))
+#define a label for order
+
+window.mainloop()
+    
+    
+
+    
+   
+    
+
